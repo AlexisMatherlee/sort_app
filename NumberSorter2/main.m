11b8277298d3fecf85d212eb74cb0c2259092f92
Alexis Matherlee <alexismatherlee@gmail.com>
2013-10-10 00:58:22 -0700

    Initial Commit

---
+++ NumberSorter2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NumberSorter2
+//
+//  Created by Alexis Matherlee on 10/10/13.
+//  Copyright (c) 2013 Alexis Matherlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ NumberSorter2Tests/NumberSorter2Tests.m
@@ -0,0 +1,34 @@
+//
+//  NumberSorter2Tests.m
+//  NumberSorter2Tests
+//
+//  Created by Alexis Matherlee on 10/10/13.
+//  Copyright (c) 2013 Alexis Matherlee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NumberSorter2Tests : XCTestCase
+
+@end
+
+@implementation NumberSorter2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
